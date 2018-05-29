1d1b752125c585f64c60f7859a8b3e23c1f522c8
hoan.lee <lonelywolf.lee@gmail.com>
2018-05-29 21:15:09 +0900

    환경 설정

---
+++ src/main/java/pro/lonelywolf/demo/kftc/api/ApiConfig.java
@@ -0,0 +1,11 @@
+package pro.lonelywolf.demo.kftc.api;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ApiConfig {
+    public static String CLIENT_ID="client-id";
+    public static String CLIENT_SECRET_KEY="client-secret-key";
+
+    public static String HOST = "https://testapi.open-platform.or.kr";
+}
