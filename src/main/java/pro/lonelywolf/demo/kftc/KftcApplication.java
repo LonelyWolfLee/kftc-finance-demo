3b33156d9fb4777d731e0a1af5e2269f0175362a
hoan.lee <lonelywolf.lee@gmail.com>
2018-05-29 17:57:52 +0900

    Added project

---
+++ src/main/java/pro/lonelywolf/demo/kftc/KftcApplication.java
@@ -0,0 +1,12 @@
+package pro.lonelywolf.demo.kftc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KftcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KftcApplication.class, args);
+    }
+}
